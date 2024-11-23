734ad22ea89352f4c5ad42e2f82462f3ec05f80b
noaamfarber <noaam.farber@gmail.com>
2024-11-23 19:17:57 +0200

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -271,38 +271,113 @@ int main() {
        
         
             break;
-    case 6:
-    int sml,chr,max;
-
-    printf("Enter a smile and cheer number:\n");
-    while(1) {
-        if(scanf("smile:%d,cheer:%d",&sml,&chr)==2&&sml>0&&chr>0&&sml!=chr)
-            break;
-        else {
+        case 6:
+    int sml, chr, max;
+    while(1)
+    {
+        printf("Enter a smile and cheer number:\n");
+        if(scanf("smile: %d, cheer: %d",&sml,&chr)!= 2||sml<=0||chr<= 0||sml==chr)
+        {
             printf("Only 2 different positive numbers in the given format are allowed for the festival, please try again:\n");
-            while(getchar()!='\n');
+            while(getchar() != '\n');
+        }
+        else
+        {
+            while(getchar() != '\n');
+            break;
         }
     }
 
-    printf("Enter maximum number for the festival:\n");
-    while(scanf("%d",&max)!=1||max<=0) {
-        printf("Only positive maximum number is allowed, please try again:\n");
-        while(getchar()!='\n');
-    }
-
-    for(int i=1;i<=max;i++) {
-        if(i%sml==0&&i%chr==0)
-            printf("Festival!\n");
-        else if(i%sml==0)
-            printf("Smile!\n");
-        else if(i%chr==0)
-            printf("Cheer!\n");
+    while(1)
+    {
+        printf("Enter maximum number for the festival:\n");
+        if(scanf("%d", &max) != 1 || max <= 0)
+        {
+            printf("Only positive maximum number is allowed, please try again:\n");
+            while(getchar() != '\n');
+        }
         else
-            printf("%d\n",i);
-    }
-    break;
-    }
+        {
+            break;
+        }
     }
 
+    for(int i = 1; i <= max; i++)
+    {
+        if(i % chr == 0 && i % sml == 0)
+        {
+            printf("Festival!\n");
+        }
+        else if(i % chr == 0)
+        {
+            printf("Cheer!\n");
+        }
+        else if(i % sml == 0)
+        {
+            printf("Smile!\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
+            break;
+        case 7:
+        printf("Thank you for your journey through Numeria!");
+        exit=0;
+            break;
+        default:
+            printf("This option is not available, please try again.\n");
+            break;
+    }
+    }
+	// Case 1: Draw Happy Face with given symbols for eyes, nose and mouse
+	/* Example:
+	* n = 3:
+	* 0   0
+	*   o
+	* \___/
+	*/
+    
+
+	// Case 2: determine whether the sum of all digits to the left of the middle digit(s)
+	// and the sum of all digits to the right of the middle digit(s) are equal
+	/* Examples:
+	Balanced: 1533, 450810, 99
+	Not blanced: 1552, 34
+	Please notice: the number has to be bigger than 0.
+	*/
+
+	// Case 3: determine whether the sum of the proper divisors (od an integer) is greater than the number itself
+	/* Examples:
+	Abudant: 12, 20, 24
+	Not Abudant: 3, 7, 10
+	Please notice: the number has to be bigger than 0.
+	*/
+
+	// Case 4: determine wether a number is a prime.
+	/* Examples:
+	This one brings joy: 3, 5, 11
+	This one does not bring joy: 15, 8, 99
+	Please notice: the number has to be bigger than 0.
+	*/
+    
+
+	// Happy numbers: Print all the happy numbers between 1 to the given number.
+	// Happy number is a number which eventually reaches 1 when replaced by the sum of the square of each digit
+	/* Examples:
+	Happy :) : 7, 10
+	Not Happy :( : 5, 9
+	Please notice: the number has to be bigger than 0.
+	*/
+
+	// Festival of Laughter: Prints all the numbers between 1 the given number:
+	// and replace with "Smile!" every number that divided by the given smile number
+	// and replace with "Cheer!" every number that divided by the given cheer number
+	// and replace with "Festival!" every number that divided by both of them
+	/* Example:
+	6, smile: 2, cheer: 3 : 1, Smile!, Cheer!, Smile!, 5, Festival!
+	*/
 	return 0;
 }
+
